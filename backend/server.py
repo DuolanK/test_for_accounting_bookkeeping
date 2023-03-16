93105f522b45dc2f2a3a51222009b2117b3431bd
DuolanK <duolan.kharitonov@mail.ru>
2023-03-16 21:42:28 +0900

    second commit

--- backend/server.py
+++ backend/server.py
@@ -3,7 +3,7 @@ import psycopg2
 from typing import List
 from pydantic import BaseModel
 import uvicorn
-from fastapi import FastAPI, UploadFile, HTTPException, Response
+from fastapi import FastAPI, UploadFile, Response
 from fastapi.middleware.cors import CORSMiddleware
 import gzip
 import io
@@ -34,19 +34,19 @@ def upload_and_add_video(file: UploadFile, video_name: str):
     print(file.filename)
     print(file.content_type)
 
-    # Compress file using gzip
+    # сжимаем файл используя gzip
     compressed_file = io.BytesIO()
     with gzip.GzipFile(fileobj=compressed_file, mode='w') as gz:
         gz.write(file.file.read())
 
-    # Upload compressed file to AWS S3
+    # загружаем файл AWS S3
     s3 = boto3.resource("s3")
     bucket = s3.Bucket(S3_BUCKET_NAME)
     bucket.upload_fileobj(compressed_file, f"{video_name}.gz", ExtraArgs={"ACL": "public-read"})
 
     uploaded_file_url = f"https://{S3_BUCKET_NAME}.s3.amazonaws.com/{video_name}.gz"
 
-    # Store URL in database
+    # Сохраняем URL в базе данных, можно конечно и без этого
     conn = psycopg2.connect(
         database="accounting", user="postgres", password="546546", host="0.0.0.0"
     )
